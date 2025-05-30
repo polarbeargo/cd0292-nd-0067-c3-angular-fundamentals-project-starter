393d38d031e7705d34b6de07b9a7cd091c56fbc0
Hsin-Wen Chang <bow1226@gmail.com>
2025-05-30 09:42:51 +0800

    feat: Implement ProductService that handles relate API calls

---
+++ my-store/src/app/services/product.service.ts
@@ -0,0 +1,25 @@
+// filepath: /src/app/services/product.service.ts
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Product } from '../models/interface';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+  private dataUrl = '/assets/data.json';
+
+  constructor(private http: HttpClient) {}
+
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.dataUrl).pipe(
+      catchError((error) => {
+        console.error('Error fetching products', error);
+        return throwError(error);
+      }),
+    );
+  }
+}
