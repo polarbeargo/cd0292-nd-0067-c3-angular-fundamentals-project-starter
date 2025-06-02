fda1b19c916d972269a1b5517b5f542437c83738
Hsin-Wen Chang <bow1226@gmail.com>
2025-06-02 13:57:41 +0800

    feat: Implement the app routing module

--- my-store/src/app/app.routes.ts
+++ my-store/src/app/app.routes.ts
@@ -1,3 +1,20 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductDetailsComponent } from './components/product-item-detail/product-item-detail.component';
+import { ConfirmationComponent } from './components/confirmation/confirmation.component';
+import { CartComponent } from './components/cart/cart.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'products', component: ProductListComponent },
+  { path: 'products/:id', component: ProductDetailsComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'confirmation', component: ConfirmationComponent },
+  { path: '**', redirectTo: '' }, // Redirect unknown routes to the main page
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
