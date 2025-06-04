323c26c1cc792918a69cb36490ccd59aa6581da8
Hsin-Wen Chang <bow1226@gmail.com>
2025-06-04 14:16:13 +0800

    feat: Change the default route

--- my-store/src/app/app.component.ts
+++ my-store/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { AppRoutingModule } from './app.routes';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, AppRoutingModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'my-store';
