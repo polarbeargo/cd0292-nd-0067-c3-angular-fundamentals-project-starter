03855e07b41d4b09368d70150e9b715f67d1688b
Hsin-Wen Chang <bow1226@gmail.com>
2025-06-14 13:33:57 +0800

    feat: Implement forms must be validated

--- my-store/src/app/components/confirmation/confirmation.component.ts
+++ my-store/src/app/components/confirmation/confirmation.component.ts
@@ -11,16 +11,36 @@ export class ConfirmationComponent {
   address: string = '';
   paymentDetails: string = '';
   isSuccess: boolean = false;
+  nameError: string = '';
+  addressError: string = '';
+  paymentError: string = '';
 
   constructor(private cartService: CartService) {}
 
   onSubmit(): void {
-    this.cartService.checkout(this.name, this.address, this.paymentDetails);
-    console.log('Confirmation Details:', {
-      name: this.name,
-      address: this.address,
-      paymentDetails: this.paymentDetails,
-    });
-    this.isSuccess = true;
+    this.validateForm();
+    if (!this.nameError && !this.addressError && !this.paymentError) {
+      this.cartService.checkout(this.name, this.address, this.paymentDetails);
+      console.log('Confirmation Details:', {
+        name: this.name,
+        address: this.address,
+        paymentDetails: this.paymentDetails,
+      });
+      this.isSuccess = true;
+    }
+  }
+
+  validateForm(): void {
+    this.nameError =
+      this.name.length < 3 ? 'Name must be at least 3 characters long.' : '';
+    this.addressError =
+      this.address.length < 5
+        ? 'Address must be at least 5 characters long.'
+        : '';
+    this.paymentError = !this.paymentDetails
+      ? 'Payment details is required.'
+      : isNaN(Number(this.paymentDetails))
+        ? 'Payment details must be a number.'
+        : '';
   }
 }
